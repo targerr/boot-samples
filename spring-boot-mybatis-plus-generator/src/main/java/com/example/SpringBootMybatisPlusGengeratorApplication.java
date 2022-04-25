fd81e1d586014694f5f12d3af98617efcc604874
wanggaoshuai <wang1416>
2022-04-25 13:41:45 +0800

    springboot 整合mybatis-plus-generator

---
+++ spring-boot-mybatis-plus-generator/src/main/java/com/example/SpringBootMybatisPlusGengeratorApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisPlusGengeratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMybatisPlusGengeratorApplication.class, args);
+    }
+
+}
