63476f4964437263b3d96916032572480484e2b9
wanggaoshuai <wang1416>
2022-05-02 17:51:06 +0800

    springboot 整合knife4j

---
+++ spring-boot-ui-knife4j/src/main/java/com/example/SpringBootUiKnife4jApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUiKnife4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUiKnife4jApplication.class, args);
+    }
+
+}
---
+++ spring-boot-ui-knife4j/src/main/java/com/example/config/Knife4jConfiguration.java
@@ -0,0 +1,41 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+/**
+ * @Author: wgs
+ * @Date 2022/4/28 16:56
+ * @Classname Knife4jConfiguration
+ * @Description
+ */
+@Configuration
+@EnableSwagger2WebMvc
+public class Knife4jConfiguration {
+
+    @Bean(value = "defaultApi2")
+    public Docket defaultApi2() {
+        Docket docket=new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(new ApiInfoBuilder()
+                        //.title("swagger-bootstrap-ui-demo RESTful APIs")
+                        .description("# swagger-bootstrap-ui-demo RESTful APIs")
+                        .termsOfServiceUrl("http://www.xx.com/")
+                        .contact("xx@qq.com")
+                        .version("1.0")
+                        .build())
+                //分组名称
+                .groupName("2.X版本")
+                .select()
+                //这里指定Controller扫描包路径
+                .apis(RequestHandlerSelectors.basePackage("com.example.controller"))
+                .paths(PathSelectors.any())
+                .build();
+        return docket;
+    }
+}
