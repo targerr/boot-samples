079ca9f63d7b7de393ef6ae5ed2d87c7591b9e48
wanggaoshuai <wang1416>
2022-05-06 11:06:15 +0800

    springboot 自定义starter测试用例

---
+++ spring-boot-starter-myhello/src/main/java/com/example/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.example.controller;
+
+import com.example.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: wgs
+ * @Date 2022/5/6 10:57
+ * @Classname HelloController
+ * @Description
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    //HelloService在我们自定义的starter中已经完成了自动配置，所以此处可以直接注入
+    @Autowired
+    private HelloService helloService;
+
+    @GetMapping("/say")
+    public String sayHello() {
+        return helloService.sayHello();
+    }
+}
