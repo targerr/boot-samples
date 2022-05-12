1428f143b5c2d63c9b0dcaa7993973d27bfd2607
wanggaoshuai <wang1416>
2022-05-12 15:21:20 +0800

    springboot 整合jwt

---
+++ spring-boot-token-jwt/src/main/java/com/example/entity/User.java
@@ -0,0 +1,17 @@
+package com.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @Author: wgs
+ * @Date 2022/5/12 14:39
+ * @Classname User
+ * @Description
+ */
+@Data
+@AllArgsConstructor
+public class User {
+    private Integer id;
+    private String name;
+}
