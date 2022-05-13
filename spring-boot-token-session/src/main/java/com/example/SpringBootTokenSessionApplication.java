d4577201f9a9798f29dea609e469209b14278359
wanggaoshuai <wang1416>
2022-05-13 10:57:52 +0800

    springboot 整合session

---
+++ spring-boot-token-session/src/main/java/com/example/SpringBootTokenSessionApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTokenSessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTokenSessionApplication.class, args);
+    }
+
+}
---
+++ spring-boot-token-session/src/main/java/com/example/controller/SessionController.java
@@ -0,0 +1,33 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @Author: wgs
+ * @Date 2022/5/13 10:30
+ * @Classname SessionController
+ * @Description
+ */
+@RestController
+@RequestMapping("/session")
+public class SessionController {
+    @GetMapping("/login")
+    public String login(String name, String age, HttpSession session) {
+        // 业务验证......
+        session.setAttribute("user", name + ":" + age);
+
+        return "login success!";
+    }
+
+    @GetMapping("/info")
+    public String info(HttpSession session) {
+        final Object user = session.getAttribute("user");
+
+        return user.toString();
+    }
+}
