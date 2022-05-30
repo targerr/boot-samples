fd34089b91d6f32da3e2ec33d1ecc836f4d38554
wanggaoshuai <wang1416>
2022-05-30 10:00:57 +0800

    springboot 集成快速实现定时任务

---
+++ spring-boot-timer-task/src/main/java/com/example/springboottimertask/SpringBootTimerTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboottimertask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTimerTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTimerTaskApplication.class, args);
+    }
+
+}
---
+++ spring-boot-timer-task/src/test/java/com/example/springboottimertask/SpringBootTimerTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottimertask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTimerTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
