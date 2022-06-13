27601a2dc2539a5e94e727e0a13b10065a98c8e3
wanggaoshuai <wang1416>
2022-06-13 23:23:54 +0800

    springboot 整合oss

---
+++ spring-boot-upload-oss/src/main/java/com/example/SpringBootUploadOssApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUploadOssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUploadOssApplication.class, args);
+    }
+
+}
