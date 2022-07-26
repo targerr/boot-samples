d03419077247315abcf84e21177fbc90608c81a0
wanggaoshuai <wang1416>
2022-07-26 14:05:27 +0800

    springboot 整合activemq实现

---
+++ spring-boot-mq-activemq/src/main/java/com/example/listener/TopicAndQueueConsumerListener.java
@@ -0,0 +1,41 @@
+package com.example.listener;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: wgs
+ * @Date 2022/7/26 14:04
+ * @Classname TopicAndQueueConsumerListener
+ * @Description
+ */
+@Component
+public class TopicAndQueueConsumerListener {
+    /**
+     * queue模式的消费者
+     * SendTo("active.queue.result") 会将此方法返回的数据, 写入到 active.queue.result 中去.
+     */
+    @SendTo("active.queue.result")
+    @JmsListener(destination="${spring.activemq.queue-name}", containerFactory="queueListener")
+    public String readP2PQActiveueue(String message) {
+        System.out.println("queue接受到：" + message);
+        return message;
+    }
+
+    /**
+     * queue模式的消费者
+     */
+    @JmsListener(destination="active.queue.result", containerFactory="queueListener")
+    public void readP2PActiveQueue1(String message) {
+        System.out.println("active.queue.result接受到：" + message);
+    }
+
+    /**
+     * topic模式的消费者
+     */
+    @JmsListener(destination="${spring.activemq.topic-name}", containerFactory="topicListener")
+    public void readTopicActiveQueue(String message) {
+        System.out.println("topic接受到：" + message);
+    }
+}
