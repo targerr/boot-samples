a8728728a54c2746584ce93925bb736103e61870
wanggs <wgshuaiit@163.com>
2022-08-15 18:50:17 +0800

    springboot 整合Alibaba相关

---
+++ spring-cloud-alibaba/src/main/java/com/example/SpringCloudAlibabaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAlibabaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAlibabaApplication.class, args);
+    }
+
+}
