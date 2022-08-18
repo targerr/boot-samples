be552c8b6fba36589d4e12eb97b1d859e2a89560
wanggs <wgshuaiit@163.com>
2022-08-18 16:05:07 +0800

    springboot 整合alibaba content注册到nacos

--- spring-cloud-alibaba/content-center/src/main/java/com/example/ContentCenterApplication.java
+++ spring-cloud-alibaba/content-center/src/main/java/com/example/ContentCenterApplication.java
@@ -2,12 +2,14 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class SpringCloudAlibabaApplication {
+@EnableDiscoveryClient
+public class ContentCenterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudAlibabaApplication.class, args);
+        SpringApplication.run(ContentCenterApplication.class, args);
     }
 
 }
