a82548cd161ad38252ef86a3fd4980a2289987bd
wanggs <wgshuaiit@163.com>
2022-08-29 14:08:31 +0800

    springboot 整合alibaba gateway过滤器相关优化

--- spring-cloud-alibaba/gateway/src/main/java/com/example/filter/TokenFilter.java
+++ spring-cloud-alibaba/gateway/src/main/java/com/example/filter/TokenFilter.java
@@ -4,13 +4,14 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.example.enums.FilterEnum;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -39,51 +40,51 @@ import java.nio.charset.StandardCharsets;
 @Slf4j
 public class TokenFilter implements GlobalFilter, Ordered {
     @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+    public Mono filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         log.info("【GatewayFilter】进入token过滤器.....");
+        ServerHttpRequest request = exchange.getRequest();
         // 请求地址
-        String reqPath = exchange.getRequest().getURI().getPath();
+        String reqPath = request.getURI().getPath();
         // 放行
         if (FilterEnum.isRelease(reqPath)) {
             return chain.filter(exchange);
         }
 
-        //获取头部
-        HttpHeaders headerNames = exchange.getRequest().getHeaders();
-        String token = StringUtils.EMPTY;
+        HttpHeaders headerNames = request.getHeaders();
+        String token = headerNames.getFirst("token");
+        if (StringUtils.isBlank(token)) {
+            log.error("【GatewayFilter】token不能为空,path:{}", reqPath);
+            return unAuth(exchange);
+        }
 
-        //获取头部参数
-        Object o = headerNames.get("token");
-
+        log.info("【GatewayFilter】token:{},path:{}", token, reqPath);
         if ("eaa1929451cd43efb3f4668eed25e3f9".equals(token)) {
             return chain.filter(exchange);
         }
+
+        // 验证token,顺延；权限校验....
 
         return unAuth(exchange);
     }
 
 
-    private Mono unAuth(ServerWebExchange serverWebExchange) {
-/*        //不允许访问，禁止访问
-        ServerHttpResponse response = exchange.getResponse();
-        //这个状态码是401
-        response.setStatusCode(HttpStatus.UNAUTHORIZED);
-        return exchange.getResponse().setComplete();*/
-
+    private Mono<Void> unAuth(ServerWebExchange serverWebExchange) {
         // 权限不够拦截
         serverWebExchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
 
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("msg","无权限");
-        jsonObject.put("statusCode",HttpStatus.UNAUTHORIZED);
-
+        jsonObject.put("msg", "无权限");
+        jsonObject.put("statusCode", HttpStatus.UNAUTHORIZED);
+        //不允许访问，禁止访问
         DataBuffer buffer = serverWebExchange.getResponse().bufferFactory().wrap(JSON.toJSONString(jsonObject).getBytes(StandardCharsets.UTF_8));
         ServerHttpResponse response = serverWebExchange.getResponse();
+        //这个状态码是401
         response.setStatusCode(HttpStatus.UNAUTHORIZED);
         //指定编码，否则在浏览器中会中文乱码
         response.getHeaders().add("Content-Type", "application/json;charset=UTF-8");
         return response.writeWith(Mono.just(buffer));
     }
+
 
     @Override
     public int getOrder() {
