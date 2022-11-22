87877a72881145d698c855c81bc04fa55d841610
wanggaoshuai <wang1416>
2022-11-22 16:17:47 +0800

    springboot整合 异步 注入异步任务管理器

---
+++ spring-boot-async/src/main/java/com/example/aop/AsyncTaskMonitor.java
@@ -0,0 +1,74 @@
+package com.example.aop;
+
+import com.example.enums.AsyncTaskStatusEnum;
+import com.example.service.AsyncTaskManager;
+import com.example.vo.AsyncTaskInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @Author: wgs
+ * @Date 2022/11/17 11:22
+ * @Classname AsyncTaskMonitor
+ * @Description 异步任务执行监控切面
+ */
+@Slf4j
+@Aspect
+@Component
+public class AsyncTaskMonitor {
+    /** 注入异步任务管理器 */
+    private final AsyncTaskManager asyncTaskManager;
+
+    public AsyncTaskMonitor(AsyncTaskManager asyncTaskManager) {
+        this.asyncTaskManager = asyncTaskManager;
+    }
+
+    /**
+     * <h2>异步任务执行的环绕切面</h2>
+     * 环绕切面让我们可以在方法执行之前和执行之后做一些 "额外" 的操作
+     * 后期使用注解代替
+     */
+    @Around("execution(* com.example.service.AsyncService.*(..))")
+    public Object taskHandle(ProceedingJoinPoint proceedingJoinPoint) {
+
+        // 获取 taskId, 调用异步任务传入的第二个参数
+        String taskId = proceedingJoinPoint.getArgs()[0].toString();
+
+        // 获取任务信息, 在提交任务的时候就已经放入到容器中了
+        AsyncTaskInfo taskInfo = asyncTaskManager.getTaskInfo(taskId);
+        log.info("异步任务监控线程 id: [{}]", taskId);
+
+        taskInfo.setStatus(AsyncTaskStatusEnum.RUNNING);
+        asyncTaskManager.setTaskInfo(taskInfo); // 设置为运行状态, 并重新放入容器
+
+        AsyncTaskStatusEnum status;
+        Object result;
+
+        try {
+            // 执行异步任务
+            result = proceedingJoinPoint.proceed();
+            status = AsyncTaskStatusEnum.SUCCESS;
+        } catch (Throwable ex) {
+            // 异步任务出现了异常
+            result = null;
+            status = AsyncTaskStatusEnum.FAILED;
+            log.error("AsyncTaskMonitor: async task [{}] is failed, Error Info: [{}]",
+                    taskId, ex.getMessage(), ex);
+        }
+
+        // 设置异步任务其他的信息, 再次重新放入到容器中
+        taskInfo.setEndTime(new Date());
+        taskInfo.setStatus(status);
+        taskInfo.setTotalTime(String.valueOf(
+                taskInfo.getEndTime().getTime() - taskInfo.getStartTime().getTime()
+        ));
+        asyncTaskManager.setTaskInfo(taskInfo);
+
+        return result;
+    }
+}
--- spring-boot-async/src/main/java/com/example/config/AsyncConfig.java
+++ spring-boot-async/src/main/java/com/example/config/AsyncConfig.java
@@ -1,5 +1,7 @@
 package com.example.config;
 
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +9,7 @@ import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.lang.reflect.Method;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
 
@@ -14,35 +17,61 @@ import java.util.concurrent.ThreadPoolExecutor;
  * @Author: wgs
  * @Date 2022/4/18 15:14
  * @Classname AsyncConfig
- * @Description 线程池类
+ * @Description 自定义异步任务线程池, 异步任务异常捕获处理器
  */
-@EnableAsync
+@EnableAsync// 开启 Spring 异步任务支持
 @Configuration
+@Slf4j
 public class AsyncConfig implements AsyncConfigurer {
     @Bean
     public Executor taskExecutor() {
-        ThreadPoolTaskExecutor threadPool = new ThreadPoolTaskExecutor();
-        //设置核心线程数
-        threadPool.setCorePoolSize(10);
-        //设置最大线程数
-        threadPool.setMaxPoolSize(100);
-        //线程池所使用的缓冲队列
-        threadPool.setQueueCapacity(10);
-        // 拒绝策略， 由当前执行线程执行
-        threadPool.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
-        //等待任务在关机时完成--表明等待所有线程执行完
-        threadPool.setWaitForTasksToCompleteOnShutdown(true);
-        // 等待时间 （默认为0，此时立即停止），并没等待xx秒后强制停止
-        threadPool.setAwaitTerminationSeconds(60);
-        //  线程名称前缀
-        threadPool.setThreadNamePrefix("async-");
-        // 初始化线程
-        threadPool.initialize();
-        return threadPool;
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
+        // 设置核心线程数
+        executor.setCorePoolSize(2);
+        // 设置最大线程数
+        executor.setMaxPoolSize(2);
+        // 线程池所使用的缓冲队列
+        executor.setQueueCapacity(2);
+        executor.setThreadNamePrefix("Qinyi-Async-");   // 这个非常重要
+
+        // 等待所有任务结果候再关闭线程池
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(60);
+        // 定义拒绝策略
+        executor.setRejectedExecutionHandler(
+                new ThreadPoolExecutor.CallerRunsPolicy()
+        );
+        // 初始化线程池, 初始化 core 线程
+        executor.initialize();
+
+        return executor;
     }
 
+    /**
+     * 知道系统中异步线程出现异常所用到的处理器
+     */
     @Override
     public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
-        return null;
+        return new AsyncExceptionHandler();
+    }
+
+    /**
+     * <h2>异步任务异常捕获处理器</h2>
+     */
+    @SuppressWarnings("all")
+    class AsyncExceptionHandler implements AsyncUncaughtExceptionHandler {
+
+        @Override
+        public void handleUncaughtException(Throwable throwable, Method method,
+                                            Object... objects) {
+
+            throwable.printStackTrace();
+            log.error("线程池错误: [{}], 方法: [{}], 参数: [{}]",
+                    throwable.getMessage(), method.getName(),
+                    JSON.toJSONString(objects));
+
+            // TODO 发送邮件或者是短信, 做进一步的报警处理
+        }
     }
 }
