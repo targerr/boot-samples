bbde03344599bab6abf41ac1e64af564548540e4
wanggaoshuai <wgshuaiit@163.com>
2023-06-28 15:42:50 +0800

    feature:swagger 配置类

---
+++ spring-cloud-commerce/commerce-service/commerce-service-config/src/main/java/com/example/config/WebMvcConfig.java
@@ -0,0 +1,45 @@
+package com.example.config;
+
+import com.example.filter.LoginUserInfoInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @Author: wgs
+ * @Date 2022/11/15 11:24
+ * @Classname WebMvcConfig
+ * @Description web mvc 配置
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+    /**
+     * <h2>添加拦截器配置</h2>
+     * */
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+
+        // 添加用户身份统一登录拦截的拦截器
+        registry.addInterceptor(new LoginUserInfoInterceptor())
+                .addPathPatterns("/**").order(0);
+    }
+
+    /**
+     * <h2>让 MVC 加载 Swagger 的静态资源</h2>
+     * */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/**").
+                addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("doc.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        super.addResourceHandlers(registry);
+    }
+}
