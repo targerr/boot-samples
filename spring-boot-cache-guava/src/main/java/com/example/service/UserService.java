bff10bdd81f2c206e9a9a684776d5cacac9eacc1
wanggaoshuai <wgshuaiit@163.com>
2023-07-26 09:56:15 +0800

    feature（spring-cloud-cache-guava）: guava缓存示例

--- spring-boot-cache-guava/src/main/java/com/example/service/UserService.java
+++ spring-boot-cache-guava/src/main/java/com/example/service/UserService.java
@@ -1,7 +1,11 @@
 package com.example.service;
 
+import cn.hutool.core.thread.ThreadUtil;
+import com.alibaba.fastjson.JSON;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
@@ -13,11 +17,13 @@ import java.util.concurrent.TimeUnit;
  * @Classname UserService
  * @Description
  */
+@Data
+@Slf4j
 public class UserService {
 
-    private static Cache<String,User> cache = CacheBuilder.newBuilder()
-            .maximumSize(100)//f放一百个数据
-            .expireAfterAccess(10, TimeUnit.MINUTES)//天数
+    private static Cache<String, User> cache = CacheBuilder.newBuilder()
+            .maximumSize(100)//放一百个数据
+            .expireAfterAccess(10, TimeUnit.SECONDS)//过期时间
             .build();
 
     public User findById(Integer id) {
@@ -27,8 +33,8 @@ public class UserService {
                 @Override
                 public User call() {
                     // 查询用户信息
-                   // return userDao.findById(id);
-                    return  null;
+                    // return userDao.findById(id);
+                    return findUserById(id);
                 }
             });
         } catch (ExecutionException e) {
@@ -37,7 +43,32 @@ public class UserService {
         return user;
     }
 
-    class User{
+    private User findUserById(Integer id) {
+        log.info("模拟数据查询用户信息~~~");
+        User user = new User();
+        user.setId(id);
+        user.setName("tom");
+        return user;
+    }
+
+    @Data
+    class User {
         private String name;
+        private Integer id;
+    }
+
+    public static void main(String[] args) {
+        UserService userService = new UserService();
+        final User user = userService.findById(1);
+        log.info(JSON.toJSONString(user, true));
+
+        ThreadUtil.sleep(2000L);
+     //  cache.invalidateAll();
+
+        User user1 = userService.findById(1);
+
+        log.info(JSON.toJSONString(user1, true));
+
+
     }
 }
