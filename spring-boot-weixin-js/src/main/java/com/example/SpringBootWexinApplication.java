fd493ca5b00ebe4cb953794c35f78abc485229b4
wanggaoshuai <wgshuaiit@163.com>
2023-08-09 13:48:27 +0800

    feature（spring-cloud-weixi-js）: 配置安全域名

---
+++ spring-boot-weixin-js/src/main/java/com/example/SpringBootWexinApplication.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: wgs
+ * @Date 2023/8/9 13:35
+ * @Classname SpringBootWexinApplication
+ * @Description
+ */
+@SpringBootApplication
+public class SpringBootWexinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWexinApplication.class, args);
+    }
+
+}
