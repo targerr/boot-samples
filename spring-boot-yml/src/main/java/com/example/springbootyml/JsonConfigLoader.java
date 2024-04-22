18bc469c6d3403ca75e0c259179fb0ce9cb9f005
wanggaoshuai <wgshuaiit@163.com>
2024-04-22 17:30:23 +0800

    feature（spring-boot-yml）: 读取json文件多种方式

--- spring-boot-yml/src/main/java/com/example/springbootyml/JsonConfigLoader.java
+++ spring-boot-yml/src/main/java/com/example/springbootyml/JsonConfigLoader.java
@@ -1,5 +1,6 @@
 package com.example.springbootyml;
 
+import cn.hutool.core.io.IoUtil;
 import cn.hutool.json.JSONUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
+
 /**
  * @Author: wgs
  * @Date 2024/4/19
@@ -29,7 +34,14 @@ public class JsonConfigLoader {
     public void init() {
         try {
             Resource resource = resourceLoader.getResource("classpath:taikang.json");
-            String content = new String(resource.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+//            String content = new String(resource.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+            Reader reader = new InputStreamReader(resource.getInputStream());
+            final String content = IoUtil.read(reader);
+
+            System.out.println("读取配置文件: " + content);
+//            final byte[] bytes = IoUtil.readBytes(inputStream);
+//            String content = new String(bytes, StandardCharsets.UTF_8);
+
             configMap = JSONUtil.parseObj(content).toBean(Map.class);
         } catch (IOException e) {
             throw new RuntimeException("Failed to load JSON config file", e);
