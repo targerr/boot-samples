56df0dd6387998906cd7e0c4dc5fed18d1eb2af3
wanggaoshuai <wgshuaiit@163.com>
2024-12-20 11:42:47 +0800

    规则引擎

---
+++ spring-boot-drools/src/main/java/com/example/springbootdrools/SpringbootDroolsApplication.java
@@ -0,0 +1,18 @@
+package com.example.springbootdrools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: wgs
+ * @Date 2024/12/20 11:41
+ * @Classname SpringbootDroolsApplication
+ * @Description
+ */
+@SpringBootApplication
+public class SpringbootDroolsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDroolsApplication.class, args);
+    }
+
+}
